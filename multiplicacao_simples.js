047f9e778d4ff92350afcaeee70b6725aad4c189
Bruna Santos de Paula <51168817+BrunaAdmComp@users.noreply.github.com>
2021-08-09 17:28:13 -0300

    Correção de Upload

---
+++ multiplicacao_simples.js
@@ -0,0 +1,7 @@
+//let valor1 = parseInt(gets()); //para uso na DIO
+//let valor2 = parseInt(gets()); //para uso na DIO
+
+let valor1 = parseInt(gets()); //para uso local
+let valor2 = parseInt(gets()); //para uso local
+let total = valor1 * valor2;
+console.log("PROD = " + total);
